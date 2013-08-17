66dab5625ade7b652a06a4d8f4b814e6d2f89133
supersuraccoon <supersuraccoon@gmail.com>
2013-08-17 16:03:35 +0800

    remove nused lib
    remove nused lib

--- CocosJSBLab-HTML5/boot-html5.js
+++ CocosJSBLab-HTML5/boot-html5.js
@@ -3,9 +3,9 @@
     var c = {
         COCOS2D_DEBUG:2, //0 to turn debug off, 1 for basic debug, and 2 for full debug
         box2d:false,
-        chipmunk:true,
-        showFPS:true,
-        loadExtension:true,
+        chipmunk:false,
+        showFPS:false,
+        loadExtension:false,
         frameRate:60,
         tag:'gameCanvas', //the dom element to run cocos2d on
         //engineDir:'../../../../cocos2d/',
